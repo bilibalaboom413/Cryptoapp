87f18cbac17e19650e1058308e04f7abfaeecdb8
Hao Chen <hche8093@uni.sydney.edu.au>
2022-07-11 11:29:21 +0800

    Navbar setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
-import { Switch, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
+import {
+	Homepage,
+	Navbar,
+	Exchanges,
+	Cryptocurrencies,
+	CryptoDetails,
+	News,
+} from "./components";
+import "./App.css";
 
 function App() {
 	return (
-		<div className="App">
-			<div className="navbar"></div>
-			<div className="main"></div>
+		<div className="app">
+			<div className="navbar">
+				<Navbar />
+			</div>
+			<div className="main">
+				<Layout>
+					<div className="routes">
+						<Routes>
+							<Route exact path="/" element={<Homepage />} />
+							<Route
+								exact
+								path="/exchanges"
+								element={<Exchanges />}
+							/>
+							<Route
+								exact
+								path="/cryptocurrencies"
+								element={<Cryptocurrencies />}
+							/>
+							<Route
+								exact
+								path="/crypto/:coinId"
+								element={<CryptoDetails />}
+							/>
+							<Route exact path="/news" element={<News />} />
+							{/* <Route exact path="/">
+								<Homepage />
+							</Route> */}
+							{/* <Route exact path="/exchanges">
+								<Exchanges />
+							</Route> */}
+							{/* <Route exact path="/cryptocurrencies">
+								<Cryptocurrencies />
+							</Route>
+							<Route exact path="/crypto/:coinId">
+								<CryptoDetails />
+							</Route>
+							<Route exact path="/news">
+								<News />
+							</Route> */}
+						</Routes>
+					</div>
+				</Layout>
+			</div>
 			<div className="footer"></div>
 		</div>
 	);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,11 +10,13 @@ import {
 	MenuOutlined,
 } from "@ant-design/icons";
 
+import icon from "../images/cryptocurrency.png";
+
 const Navbar = () => {
 	return (
 		<div className="nav-container">
 			<div className="logo-container">
-				<Avatar />
+				<Avatar src={icon} size="large" />
 				<Typography.Title level={2} className="logo">
 					<Link to="/">Crytoverse</Link>
 				</Typography.Title>
@@ -22,6 +24,20 @@ const Navbar = () => {
 
       </Button> */}
 			</div>
+			<Menu className="dark">
+				<Menu.Item icon={<HomeOutlined />}>
+					<Link to="/">Home</Link>
+				</Menu.Item>
+				<Menu.Item icon={<FundOutlined />}>
+					<Link to="/cryptocurrencies">Cryptocurrencies</Link>
+				</Menu.Item>
+				<Menu.Item icon={<MoneyCollectOutlined />}>
+					<Link to="/exchanges">Exchanges</Link>
+				</Menu.Item>
+				<Menu.Item icon={<BulbOutlined />}>
+					<Link to="/news">News</Link>
+				</Menu.Item>
+			</Menu>
 		</div>
 	);
 };
